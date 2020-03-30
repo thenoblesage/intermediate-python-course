ae4109d469363ee656ccca7f16a6f492ae72433b
Dijhon Tulloch <dijhontulloch@Dijhons-MacBook-Air.local>
2020-03-30 06:43:14 -0500

    Added conditionals.

--- dice_roller.py
+++ dice_roller.py
@@ -6,7 +6,12 @@ def main():
     for i in range (0, dicerolls):
         roll = random.randint(1, 6)
         sum += roll
-        print(f'You rolled a {roll}')
+        if roll == 1:
+            print(f'You rolled a {roll}! Critical Fail')
+        elif roll == 6:
+            print(f'You rolled a {roll}! Critical Success')
+        else:
+            print(f'You rolled a {roll}')
     print(f'You have rolled a total of {sum}')
 
 
